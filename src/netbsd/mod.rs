f086ef2f4c802a8cc37a2ee7db0f0231b7961c1e
ci4ic4 <ci4ic4@gmail.com>
2021-03-25 13:06:57 +0000

    Update mod.rs
    /usr/sbin/envstat, at least under -current 9.99.81, inserts a space before the percent charge when the values for this are low; I haven't tested all possible combinations, but on my old laptop with a battery almost non-functional I get
      charge:     0.000                      3.571%   1.786%   Ah ( 0.00%) 
    
    The space before 0.00% causes trouble with the conversion.

--- src/netbsd/mod.rs
+++ src/netbsd/mod.rs
@@ -44,6 +44,7 @@ impl BatteryReadout for NetBSDBatteryReadout {
                             .get(1)
                             .map_or("", |m| m.as_str())
                             .to_string()
+                            .replace(" ","")
                             .replace("%", "");
                         let percentage_f = percentage.parse::<f32>().unwrap();
                         let percentage_i = percentage_f.round() as u8;
