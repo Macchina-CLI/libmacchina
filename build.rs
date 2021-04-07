c11648f2fe33745668057b1733c6dbc4a3c99d77
Uttarayan Mondal <uttarayan21@gmail.com>
2021-04-07 18:49:57 +0530

    Remove build_linux since not needed.
    build_linux() checks if current device is openwrt.
    Which is not required since it would be cross compiled.

--- build.rs
+++ build.rs
@@ -1,18 +1,4 @@
 use std::env;
-use std::io;
-
-fn distribution() -> io::Result<String> {
-    #[cfg(target_os = "linux")]
-    {
-        use os_release::OsRelease;
-        let content = OsRelease::new()?;
-        return Ok(content.name.to_lowercase());
-    }
-    return Err(io::Error::new(
-        io::ErrorKind::InvalidData,
-        "Not a linux system",
-    ));
-}
 
 fn build_windows() {
     #[cfg(windows)]
@@ -27,19 +13,10 @@ fn build_macos() {
     println!("cargo:rustc-link-lib=framework=IOKit");
 }
 
-fn build_linux() {
-    if let Ok(distro) = distribution() {
-        if distro == String::from("openwrt") {
-            println!("cargo:rustc-cfg=feature=openwrt");
-        }
-    }
-}
-
 fn main() {
     match env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
         Ok("macos") => build_macos(),
         Ok("windows") => build_windows(),
-        Ok("linux") => build_linux(),
         _ => {}
     }
 }
