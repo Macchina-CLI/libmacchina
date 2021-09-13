b90a45632b7652c05ed2f230363ba6e606049690
grtcdr <mail@grtcdr.rocks>
2021-09-13 05:55:02 +0100

    [netbsd] build.rs: leave early

--- build.rs
+++ build.rs
@@ -17,6 +17,7 @@ fn main() {
     match env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
         Ok("macos") => build_macos(),
         Ok("windows") => build_windows(),
+        Ok("netbsd") => return,
         _ => {}
     }
 
