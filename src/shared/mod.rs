fc2151c21e872f82bee1866b0d3d6bf901610f2e
grtcdr <ba.tahaaziz@gmail.com>
2021-10-18 03:01:21 +0100

    expose shared::cpu_usage to freebsd module

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -205,7 +205,7 @@ pub(crate) fn cpu_model_name() -> String {
     }
 }
 
-#[cfg(any(target_os = "macos", target_os = "netbsd"))]
+#[cfg(any(target_os = "freebsd", target_os = "macos", target_os = "netbsd"))]
 pub(crate) fn cpu_usage() -> Result<usize, ReadoutError> {
     let nelem: i32 = 1;
     let mut value: f64 = 0.0;
