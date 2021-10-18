6f259a02f391a432aec57c65aec77fb4c3a6e646
grtcdr <ba.tahaaziz@gmail.com>
2021-10-18 02:26:43 +0100

    impl: hostname readout

--- src/freebsd/mod.rs
+++ src/freebsd/mod.rs
@@ -1,11 +1,14 @@
 use crate::traits::*;
 use byte_unit::AdjustedByte;
+use sysctl::{Ctl, Sysctl};
 
 pub struct FreeBSDBatteryReadout;
 
 pub struct FreeBSDKernelReadout;
 
-pub struct FreeBSDGeneralReadout;
+pub struct FreeBSDGeneralReadout {
+    hostname_ctl: Option<Ctl>,
+};
 
 pub struct FreeBSDMemoryReadout;
 
@@ -19,11 +22,11 @@ impl BatteryReadout for FreeBSDBatteryReadout {
     }
 
     fn percentage(&self) -> Result<u8, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn status(&self) -> Result<BatteryState, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 }
 
@@ -33,93 +36,100 @@ impl KernelReadout for FreeBSDKernelReadout {
     }
 
     fn os_release(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn os_type(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn pretty_kernel(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 }
 
 impl GeneralReadout for FreeBSDGeneralReadout {
     fn new() -> Self {
-        FreeBSDGeneralReadout
+        FreeBSDGeneralReadout {
+            hostname_ctl: Ctl::new("kernel.hostname").ok(),
+        }
     }
 
     fn resolution(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn backlight(&self) -> Result<usize, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn machine(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn local_ip(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn username(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn hostname(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Ok(
+            self
+            .hostname_ctl
+            .as_ref()
+            .ok_or(ReadoutError::MetricNotAvailable)?
+            .value_string().unwrap())
     }
 
     fn distribution(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn desktop_environment(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn window_manager(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn terminal(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn shell(&self, shorthand: ShellFormat, kind: ShellKind) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn cpu_model_name(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn cpu_cores(&self) -> Result<usize, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn cpu_physical_cores(&self) -> Result<usize, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn cpu_usage(&self) -> Result<usize, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn uptime(&self) -> Result<usize, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn os_name(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn disk_space(&self) -> Result<(AdjustedByte, AdjustedByte), ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 }
 
@@ -129,15 +139,15 @@ impl MemoryReadout for FreeBSDMemoryReadout {
     }
 
     fn total(&self) -> Result<u64, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn free(&self) -> Result<u64, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn used(&self) -> Result<u64, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 }
 
@@ -147,24 +157,20 @@ impl ProductReadout for FreeBSDProductReadout {
     }
 
     fn version(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn vendor(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 
     fn product(&self) -> Result<String, ReadoutError> {
-        todo!()
+        Err(ReadoutError::MetricNotAvailable)
     }
 }
 
 impl PackageReadout for FreeBSDPackageReadout {
     fn new() -> Self {
         FreeBSDPackageReadout
-    }
-
-    fn count_pkgs(&self) -> Vec<(PackageManager, usize)> {
-        todo!()
     }
 }
