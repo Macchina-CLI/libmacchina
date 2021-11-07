ff217febaf15160bac4220c3cdc680996db126ea
FantasyTeddy <matthiasbaer@bluewin.ch>
2021-11-07 11:55:57 +0100

    Update the 'windows' crate from 0.4.0 -> 0.25.0

--- build.rs
+++ build.rs
@@ -1,12 +1,5 @@
 use std::env;
 use vergen::{Config, ShaKind};
-fn build_windows() {
-    #[cfg(windows)]
-    windows::build!(
-        windows::win32::windows_programming::{GetUserNameA, GetComputerNameExA, GetTickCount64},
-        windows::win32::system_services::{GlobalMemoryStatusEx, GetSystemPowerStatus},
-    );
-}
 
 fn build_macos() {
     println!("cargo:rustc-link-lib=framework=Foundation");
@@ -16,7 +9,6 @@ fn build_macos() {
 fn main() {
     match env::var("CARGO_CFG_TARGET_OS").as_ref().map(|x| &**x) {
         Ok("macos") => build_macos(),
-        Ok("windows") => build_windows(),
         Ok("netbsd") => return,
         _ => {}
     }
