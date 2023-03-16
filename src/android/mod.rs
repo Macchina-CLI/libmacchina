67f7a527b0f0fa1f6cc39235b832251658cf9912
Adrian Groh <adrian.groh@t-online.de>
2023-03-16 17:05:48 +0100

    Implement Android `os_name()` detection

--- src/android/mod.rs
+++ src/android/mod.rs
@@ -282,7 +282,12 @@ impl GeneralReadout for AndroidGeneralReadout {
     }
 
     fn os_name(&self) -> Result<String, ReadoutError> {
-        Err(ReadoutError::NotImplemented)
+        match android_properties::getprop("ro.build.version.release").value() {
+            Some(version) => Ok("Android ".to_string() + &version),
+            None => Err(ReadoutError::Other(
+                "Failed to get Android version".to_string(),
+            )),
+        }
     }
 
     fn disk_space(&self) -> Result<(u128, u128), ReadoutError> {
